667f936dd384e9055dc7877e02d2f5cfa9a01441
brookelove <brookelove@Brookes-iMac-2.local>
2023-02-03 11:49:08 -0800

    creating pages and components needed

---
+++ src/components/Forcast.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Forcast = () => {
+  return (
+    <div>
+      <h1>Forcast Area</h1>
+    </div>
+  );
+};
+export default Forcast;
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <h1>Navbar</h1>
+    </nav>
+  );
+};
+
+export default Navbar;
