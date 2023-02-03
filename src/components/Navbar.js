4df03bc68d6c30d06dad65a5cc64faaa88f62923
brookelove <brookelove@Brookes-iMac-2.local>
2023-02-03 12:01:33 -0800

    creating a search input:

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const [searchBar, setSearchBar] = useState("");
+  const handleChange = (e) => {
+    e.preventDefault();
+    setSearchBar(e.target.value);
+  };
+  //   if (searchBar.length > 0) {
+  //     return searchBar;
+  //   }
+  //   console.log(searchBar);
   return (
     <nav>
-      <h1>Navbar</h1>
+      <h1>w.</h1>
+      <input
+        // type={search}
+        placeholder="San Diego"
+        onChange={handleChange}
+        value={searchBar}
+      ></input>
+      <button>Search</button>
     </nav>
   );
 };
