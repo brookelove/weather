42443addaa4b727834e00c8e63d7fc280d573def
Brooke Love <savienloveb@gmail.com>
2023-02-18 14:05:51 -0500

    chaning structure of the page

--- src/components/Forcast.jsx
+++ src/components/Forcast.jsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, {useState}from "react";
 import Navbar from "../components/Navbar"
 import "../assets/css/components/Forcast.css";
 const Forcast = () => {
+  const [searchBar, setSearchBar] = useState("");
+  const handleChange = (e) => {
+    e.preventDefault();
+    setSearchBar(e.target.value);
+  };
   return (
     <div>
-      <Navbar/>
+      <div className="searchContainer bigSearch">
+                <input
+                placeholder="San Diego"
+                onChange={handleChange}
+                value={searchBar}
+                className="citySearch "
+                ></input>
+                <button className="searchBttn">SEARCH</button>
+            </div>
       <section className="forcastContianer">
         <div className="cardContainer">
           <div className="iconContainer"></div>
--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -2,23 +2,15 @@
 import React, {useState} from "react";
 import "../assets/css/components/Landing.css"
 const Landing = () => {
-    const [searchBar, setSearchBar] = useState("");
-  const handleChange = (e) => {
-    e.preventDefault();
-    setSearchBar(e.target.value);
-  };
+    
     return(
         <div className="landingContainer">
+            <div className="header">
             <h1 className="landingPageH1">w.</h1>
-            <div className="searchContainer">
-                <input
-                placeholder="San Diego"
-                onChange={handleChange}
-                value={searchBar}
-                className="citySearch"
-                ></input>
-                <button className="searchBttn">SEARCH</button>
+            {/* possibly geolocate the name of the local city */}
+                <h3 className="link">GET WEATHER</h3>
             </div>
+            
         </div>
     )
 }
