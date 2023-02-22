5ff566ec6e46c29e3819ad8b5b060f7e1819342d
brookelove <brookelove@Brookes-iMac-2.local>
2023-02-21 22:00:32 -0800

    adding css and functionality with date

--- src/components/Forcast.jsx
+++ src/components/Forcast.jsx
@@ -2,6 +2,23 @@ import React, {useState}from "react";
 import Navbar from "../components/Navbar"
 import "../assets/css/components/Forcast.css";
 const Forcast = () => {
+  const axios = require("axios");
+
+const options = {
+  method: 'GET',
+  url: 'https://weatherapi-com.p.rapidapi.com/future.json',
+  params: {q: 'London', dt: '2022-12-25'},
+  headers: {
+    'X-RapidAPI-Key': '2962b99f5emshdc16a10ed6cefa9p1375cdjsne2b97985b9dd',
+    'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
+  }
+};
+
+axios.request(options).then(function (response) {
+	console.log(response.data);
+}).catch(function (error) {
+	console.error(error);
+});
   const [searchBar, setSearchBar] = useState("");
   const handleChange = (e) => {
     e.preventDefault();
@@ -16,9 +33,15 @@ const Forcast = () => {
                 value={searchBar}
                 className="citySearch "
                 ></input>
-                <button className="searchBttn">SEARCH</button>
+                <button className="searchBttn" id="searchCity">SEARCH</button>
             </div>
       <section className="forcastContianer">
+      <div className="cardContainer big">
+          <div className="iconContainer"></div>
+          <p>Temp:</p>
+          <p>Feels Like:</p>
+          <p>Humidity:</p>
+        </div>
         <div className="cardContainer">
           <div className="iconContainer"></div>
           <p>Temp:</p>
--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,14 +1,18 @@
 
 import React, {useState} from "react";
 import "../assets/css/components/Landing.css"
+import { DateTime } from "luxon";
 const Landing = () => {
-    
+    let now = DateTime.now()
+    const dateTime = now.toLocaleString(DateTime.DATETIME_MED);
+
+    // console.log(navigator.geolocation);
     return(
         <div className="landingContainer">
             <div className="header">
             <h1 className="landingPageH1">w.</h1>
-            {/* possibly geolocate the name of the local city */}
-                <h3 className="link">GET WEATHER</h3>
+            <h4>{dateTime}</h4>
+            <h3 className="link">GET WEATHER</h3>
             </div>
             
         </div>
